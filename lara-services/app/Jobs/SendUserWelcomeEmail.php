3722970b941a2d8cc7a457429ff58501588e0cb7
Michael Chrisco <michaelc@cvtrust.org>
2016-06-27 10:41:55 -0700

    Simple welcome email service

---
+++ lara-services/app/Jobs/SendUserWelcomeEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Services\SendWelcomeEmailService;
+class SendUserWelcomeEmail extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+      (new SendWelcomeEmailService($this->user))->call();
+    }
+}
